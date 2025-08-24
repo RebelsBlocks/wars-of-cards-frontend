f0d9ab0a9fe93dd41ff56708fc352c0b135fd079
Crans <108697172+0xcrans@users.noreply.github.com>
2025-08-24 17:41:48 +0200

    Add files via upload
    https://github.com/RebelsBlocks/wars-of-cards-frontend/issues/33

---
+++ src/pages/Vanessa.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import type { NextPage } from 'next';
+import { Vanessa } from '../components/Vanessa';
+
+const VanessaPage: NextPage = () => {
+  return <Vanessa />;
+};
+
+export default VanessaPage;
