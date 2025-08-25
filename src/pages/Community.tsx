903babcd5bfe699bfc4bfae9ad4c6dc5e1fca754
Crans <108697172+0xcrans@users.noreply.github.com>
2025-08-25 16:02:41 +0200

    Add files via upload

---
+++ src/pages/Community.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import type { NextPage } from 'next';
+import Community from '@/components/Community';
+
+const CommunityPage: NextPage = () => {
+  return <Community />;
+};
+
+export default CommunityPage;
