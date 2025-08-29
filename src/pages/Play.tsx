83484ba84cf3afc6c1d74fd1ac1371edc97f2d41
Crans <108697172+0xcrans@users.noreply.github.com>
2025-08-29 13:13:55 +0200

    Update Play.tsx

--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 const PlayPage: NextPage = () => {
   return (
     <div className="mx-auto max-w-4xl md:max-w-5xl px-3 md:px-4 py-6">
-      <h2 className="mb-4 text-2xl font-semibold text-[rgb(237,201,81)]">Play</h2>
+      <h2 className="mb-4 text-2xl font-semibold text-[rgb(237,201,81)]">BLACKJACK</h2>
       <div className="card">
         <p className="text-[rgba(237,201,81,0.8)]">Coming soon!</p>
       </div>
